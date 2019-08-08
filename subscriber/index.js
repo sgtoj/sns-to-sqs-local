aee2f968a15a8ba184bce09e0dcf9a82c4354972
Brian Ojeda <brian.ojeda@outlook.com>
2019-08-08 18:17:14 -0400

    update devtools settings

--- subscriber/index.js
+++ subscriber/index.js
@@ -1,7 +1,7 @@
 //=============================================================================
 //
 // This script starts local web server, creates a public tunnel connection it
-// via ngrok, and subscribes the public accessible address to a SNS topic.SNS
+// via ngrok, and subscribes the public accessible address to a SNS topic. SNS
 // messages sent to the web server are forwarded to a SQS queue.
 //
 //=============================================================================
